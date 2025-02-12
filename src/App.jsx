4b9dc2fc353915f17cf588e15c63cee22fcf8992
Yosef <yosefdejene747@gmail.com>
2025-02-12 10:21:15 +0300

    zegocloud api call

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,65 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-import "tailwindcss";
-import Navbar from './components/Navbar';
+import * as React from 'react';
+import { ZegoUIKitPrebuilt } from '@zegocloud/zego-uikit-prebuilt';
 
-function App() {
-  const [count, setCount] = useState(0)
 
-  return (
-    
-    <div>
-      <Navbar/>
-    </div>
-  )
+function randomID(len) {
+  let result = '';
+  if (result) return result;
+  var chars = '12345qwertyuiopasdfgh67890jklmnbvcxzMNBVCZXASDQWERTYHGFUIOLKJP',
+    maxPos = chars.length,
+    i;
+  len = len || 5;
+  for (i = 0; i < len; i++) {
+    result += chars.charAt(Math.floor(Math.random() * maxPos));
+  }
+  return result;
 }
 
-export default App
+export function getUrlParams(
+  url = window.location.href
+) {
+  let urlStr = url.split('?')[1];
+  return new URLSearchParams(urlStr);
+}
+
+export default function App() {
+      const roomID = getUrlParams().get('roomID') || randomID(5);
+      let myMeeting = async (element) => {
+     // generate Kit Token
+      const appID = 412980267;
+      const serverSecret = "1d1fc88c1b2a73a1ed1947a579c127bc";
+      const kitToken =  ZegoUIKitPrebuilt.generateKitTokenForTest(appID, serverSecret, roomID,  randomID(5),  randomID(5));
+
+    
+     // Create instance object from Kit Token.
+      const zp = ZegoUIKitPrebuilt.create(kitToken);
+      // start the call
+      zp.joinRoom({
+        container: element,
+        sharedLinks: [
+          {
+            name: 'Personal link',
+            url:
+             window.location.protocol + '//' + 
+             window.location.host + window.location.pathname +
+              '?roomID=' +
+              roomID,
+          },
+        ],
+        scenario: {
+          mode: ZegoUIKitPrebuilt.GroupCall, // To implement 1-on-1 calls, modify the parameter here to [ZegoUIKitPrebuilt.OneONoneCall].
+        },
+      });
+
+    
+  };
+
+  return (
+    <div
+      className="myCallContainer"
+      ref={myMeeting}
+      style={{ width: '100vw', height: '100vh' }}
+    >
+    </div>
+  );
+}
